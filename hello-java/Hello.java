c78a20d9c6a4cb459c4331163b259cfa5e6ce251
Maxwell Tomlinson <mtomlin1@samford.edu>
2026-04-13 09:06:39 -0500

    Third commit - print number 3

--- hello-java/Hello.java
+++ hello-java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("print number 2");
+        System.out.println("print number 3");
 }
 }
